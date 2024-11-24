662e2132df931ef0e3c301c9860f2a6d990f6ab2
tobia1k <113366743+tobia1k@users.noreply.github.com>
2024-11-24 17:39:38 -0500

    Add calculator skeleton
    Added the basic layout in HTML for the calculator and basic
    arithmetic functions in calc.js as well as some very basic
    styling in style.css.

---
+++ calc.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(num1, op, num2) {
+    switch(op) {
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+        default:
+            break;
+    }
+}
