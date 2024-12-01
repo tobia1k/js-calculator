338a5875424da64070c8f4966f1e22f891e6e5d7
tobia1k <113366743+tobia1k@users.noreply.github.com>
2024-12-01 18:17:00 -0500

    Add Basic Functionality
    The user can now perform basic arithmetic operations by entering
    one number, clicking an operator button, and entering a second
    number followed by the equals button. The user can also perform
    further operations on numbers by clicking the operator again and
    entering a number. The first number in the new operation will
    be the number that was displayed after initially clicking the
    equals button.
    
    There is more debugging to be done. For example, handle divide
    by zero.

--- calc.js
+++ calc.js
@@ -1,4 +1,31 @@
+const button = document.querySelector("input[type=button]")
+const display = document.getElementById("display");
+let first = 0;
+let op = "";
+let second = 0;
+
+document.addEventListener("click", (event) => {
+
+    if (event.target.id === "clear") {
+        display.value = "";
+    }
+    else if (event.target.className === "number") {
+        display.value += event.target.value;
+    }
+    else if (event.target.className === "operator") {
+        first = Number(display.value);
+        op = event.target.value;
+        display.value = "";
+    }
+    else if (event.target.id === "equals") {
+        let second = Number(display.value);
+        console.log(first + ", " + op + ", " + second);
+        display.value = operate(first, op, second);
+    }
+});
+
 function add(a, b) {
+    console.log(typeof a);
     return a + b;
 }
 
@@ -14,20 +41,17 @@ function divide(a, b) {
     return a / b;
 }
 
+
 function operate(num1, op, num2) {
     switch(op) {
         case '+':
-            add(num1, num2);
-            break;
+            return add(num1, num2);
         case '-':
-            subtract(num1, num2);
-            break;
+            return subtract(num1, num2);
         case '*':
-            multiply(num1, num2);
-            break;
+            return multiply(num1, num2);
         case '/':
-            divide(num1, num2);
-            break;
+            return divide(num1, num2);
         default:
             break;
     }
