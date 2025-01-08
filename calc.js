2541fa623b4c444f3ee15d4e7c849e6b767acad5
tobia1k <113366743+tobia1k@users.noreply.github.com>
2025-01-07 19:51:38 -0500

    Fix bugs with calculator
    The calculator now only evaluates a single pair of numbers at a
    time. Entering a number, then operator, then another number, then
    another operator will cause the calculator to display the result
    of the first operation. Entering another number followed by = or
    another operator will show the result of the second operation and
    so on.
    
    Numbers are now rounded to up to 8 decimal places by adding
    +(operate(num1, op, num2).toFixed(8).
    
    Pressing = before completing a full operation will now result in
    the calculator displaying the first number or 0 depending on
    whether the user entered a number first or not. If the user enters
    a number and an operator, then clicks =, the calculator will assume
    the second number is 0 and display the result. This is a problem if
    the user uses the * or especially the / operator. This will be worked
    out next.

--- calc.js
+++ calc.js
@@ -1,31 +1,54 @@
-const button = document.querySelector("input[type=button]")
 const display = document.getElementById("display");
 let first = 0;
 let op = "";
 let second = 0;
+let displayToBeCleared = false;
 
 document.addEventListener("click", (event) => {
 
-    if (event.target.id === "clear") {
+    if (event.target.id === "clear" || displayToBeCleared) {
         display.value = "";
+        first = 0;
+        op = "";
+        second = 0;
+        displayToBeCleared = false;
     }
-    else if (event.target.className === "number") {
+    if (event.target.className === "number") {
         display.value += event.target.value;
     }
     else if (event.target.className === "operator") {
-        first = Number(display.value);
-        op = event.target.value;
-        display.value = "";
+        if (op === "") {
+            first = Number(display.value);
+            op = event.target.value;
+            display.value = "";
+        }
+        else {
+            // Working on catching divide by zero error
+            second = Number(display.value);
+            console.log("abogus");
+            display.value = operate(first, op, second);
+            first = Number(display.value);
+            second = 0;
+            op = event.target.value;
+            displayToBeCleared = true;
+        }
     }
     else if (event.target.id === "equals") {
-        let second = Number(display.value);
-        console.log(first + ", " + op + ", " + second);
-        display.value = operate(first, op, second);
+        if (op !== "") {
+            second = Number(display.value);
+            console.log(first + ", " + op + ", " + second);
+            display.value = +(operate(first, op, second)).toFixed(8);
+            displayToBeCleared = true;
+        }
+        else {
+            if (first !== 0) {
+                display.value = first;
+            }
+        }
     }
 });
 
 function add(a, b) {
-    console.log(typeof a);
     return a + b;
 }
 
@@ -38,7 +61,12 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    if (b === 0) {
+        return "Cannot divide by zero."
+    }
+    else {
+        return a / b;
+    }
 }
 
 
