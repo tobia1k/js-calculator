4e3dfd7f79878111f5b17670bea1bbfc14001967
tobia1k <113366743+tobia1k@users.noreply.github.com>
2025-01-11 11:34:32 -0500

    Add style to elements
    Added extra styling to elements to make it a bit more user-friendly.
    This includes color-coding the buttons. The operator that is selected
    also is highlighted until the operation is complete.

--- calc.js
+++ calc.js
@@ -2,65 +2,102 @@ const display = document.getElementById("display");
 let first = 0;
 let op = "";
 let second = 0;
-let displayToBeCleared = false;
+let finishOp = false;
 
 document.addEventListener("click", (event) => {
 
-    if (event.target.id === "clear" || displayToBeCleared) {
+    if (event.target.id === "clear") {
         display.value = "";
         first = 0;
         op = "";
         second = 0;
-        displayToBeCleared = false;
+        document.getElementById("decimal").disabled = false;
+    }
+    if (event.target.id === "backspace") {
+        let text = display.value;
+        text = text.substr(0, text.length - 1);
+        display.value = text;
     }
     if (event.target.className === "number") {
+        if (finishOp) {
+            display.value = "";
+            finishOp = false;
+        }
+        if (event.target.id === "decimal") {
+            document.getElementById("decimal").disabled = true;
+        }
         display.value += event.target.value;
     }
     else if (event.target.className === "operator") {
+        document.getElementById("decimal").disabled = false;
+        
         if (op === "") {
             first = Number(display.value);
             op = event.target.value;
             display.value = "";
         }
         else {
-            // Working on catching divide by zero error
-            second = Number(display.value);
-            console.log("abogus");
-            display.value = operate(first, op, second);
-            first = Number(display.value);
-            second = 0;
-            op = event.target.value;
-            displayToBeCleared = true;
+            if (display.value != "") {
+                second = Number(display.value);
+                let answer = operate(first, op, second)
+                if (typeof answer === "number") {
+                    display.value = +(answer).toFixed(8);
+                }
+                else {
+                    display.value = answer;
+                }
+                first = Number(display.value);
+                second = 0;
+                op = event.target.value;
+                display.value = "";
+            }
+            else {
+                op = event.target.value;
+            }
         }
+        event.target.style['background'] = 'gray';
     }
     else if (event.target.id === "equals") {
+        document.getElementById("decimal").disabled = false;
         if (op !== "") {
             second = Number(display.value);
-            console.log(first + ", " + op + ", " + second);
-            display.value = +(operate(first, op, second)).toFixed(8);
-            displayToBeCleared = true;
+            let answer = operate(first, op, second)
+            if (typeof answer === "number") {
+                display.value = +(answer).toFixed(8);
+                first = Number(display.value);
+                op = "";
+                second = 0;
+            }
+            else {
+                display.value = answer;
+            }
         }
         else {
             if (first !== 0) {
                 display.value = first;
             }
         }
+        finishOp = true;
     }
 });
 
 function add(a, b) {
+    document.getElementById("add").style['backgroundColor'] = null;
     return a + b;
 }
 
 function subtract(a, b) {
+    document.getElementById("subtract").style['backgroundColor'] = null;
     return a - b;
 }
 
 function multiply(a, b) {
+    document.getElementById("multiply").style['backgroundColor'] = null;
     return a * b;
 }
 
 function divide(a, b) {
+    document.getElementById("divide").style['backgroundColor'] = null;
     if (b === 0) {
         return "Cannot divide by zero."
     }
